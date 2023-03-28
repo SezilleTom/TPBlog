dfca65613ae78a3e5f9b34d625c623573a1ca207
SezilleTom <126882254+SezilleTom@users.noreply.github.com>
2023-03-28 11:29:57 +0200

    Add files via upload

---
+++ BLOG/app.php
@@ -0,0 +1,9 @@
+const articlesContainer = document.getElementById('articles');
+
+// Liste des articles
+const articles = [
+  {
+    title: 'Premier article',
+    content: '',
+    date: '15 mars 2023'
+  },
---
+++ BLOG/blog.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mon Blog</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Mon Blog</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">À propos</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h2>Derniers articles</h2>
+        <div id="articles">
+        </div>
+      </section>
+      <aside>
+        <h2>À propos</h2>
+        <p>Mon blog de tom sezille</p>
+      </aside>
+    </main>
+
+    <script src="app.js"></script>
+  </body>
+</html>
